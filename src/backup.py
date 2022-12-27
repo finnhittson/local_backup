29075f5c39061db66f2db6b587ba77c0699dee36
finnhittson <hittson.finn@gmail.com>
2022-12-26 18:47:47 -0900

    backup.py: add recursive method to cycle through directories

--- src/backup.py
+++ src/backup.py
@@ -1,2 +1,21 @@
 import os, string
+import shutil
+
 available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+path = 'C:/Users/hitts/Desktop/'
+items = os.listdir(path)
+backup_path = 'D:/'
+
+def check_items(items, path):
+	for item in items:
+		if os.path.isdir(path + item):
+			new_path = path + item
+			check_items(os.listdir(new_path), new_path)
+		elif os.path.splitext(item)[1] != '.ini':
+			new_path = path + item
+			check_file(item, new_path)
+
+def check_file(item, path):
+	
+
+check_items(items, path)
