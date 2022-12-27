1fc41fd8ad3b35e4ce7b6ad5e5c996900aaac113
finnhittson <hittson.finn@gmail.com>
2022-12-26 20:00:17 -0900

    backup.py: copy files not present to backup drive

--- src/backup.py
+++ src/backup.py
@@ -1,21 +1,41 @@
 import os, string
 import shutil
+import time
 
-available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
-path = 'C:/Users/hitts/Desktop/'
-items = os.listdir(path)
-backup_path = 'D:/'
+def check_items(items, path, toplevel, lu_time, ignore_files, ignore_dirs):
 
-def check_items(items, path):
+	def backup_dir(dir_name, path, mtime):
+		if dir_name not in os.listdir(path) or mtime > lu_time:
+			os.mkdir(path + dir_name)
+			print('copied dir')
+
+
+	def backup_item(item, source, destination, mtime):
+		if item not in os.listdir(destination) or mtime > lu_time:
+			shutil.copy(source, destination)
+			print('copied file')
+
 	for item in items:
-		if os.path.isdir(path + item):
-			new_path = path + item
-			check_items(os.listdir(new_path), new_path)
-		elif os.path.splitext(item)[1] != '.ini':
-			new_path = path + item
-			check_file(item, new_path)
+		if os.path.isdir(path + item) and item not in ignore_dirs:
+			backup_dir(item, toplevel, os.path.getmtime(path + item))
+			new_path = path + item + "/"
+			check_items(os.listdir(new_path), new_path, toplevel + item + "/", lu_time, ignore_files, ignore_dirs)
+		elif os.path.splitext(item)[1] != '.ini' and item not in ignore_files:
+			backup_item(item, path + item, toplevel, os.path.getmtime(path + item))
 
-def check_file(item, path):
-	
-
-check_items(items, path)
+if __name__ == '__main__':
+	run = 1
+	ignore_files = []
+	ignore_dirs = ['github']
+	backup_dirs = ['Desktop/']
+	path = 'C:/Users/hitts/'
+	backup_path = 'D:/'
+	while run:
+		lu_time = time.time()
+		available_drives = ['%s:/' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+		if backup_path in available_drives:
+			for backup_dir in backup_dirs:
+				items = os.listdir(path + backup_dir)
+				check_items(items, path + backup_dir, backup_path, lu_time, ignore_files, ignore_dirs)
+		#time.sleep(3600)
+		run = 0
