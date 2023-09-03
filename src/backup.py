945f8e772c7bf786ecca82cae2e8ca2ecba6be8a
finnhittson <hittson.finn@gmail.com>
2023-09-03 11:24:33 -0800

    Fix file deletion and log update bug

--- src/backup.py
+++ src/backup.py
@@ -1,60 +1,127 @@
 import os, string
 import shutil
 import time
+from datetime import datetime
+import argparse
+import re
 
-def check_items(path, toplevel, lu_time, ignore_files, ignore_dirs):
+#last_upt = 0
+
+# specify defaults by manually changing lists
+ignore_dirs = ['github', 'adobe', 'arduino', 'custom office templates', 'for all mankind']
+ignore_files = []
+target_dirs = ['Documents', 'Desktop']
+
+def check_items(path, top_level):
 	items = os.listdir(path)
+	update_log = []
 
-	def backup_dir(dir_name, path, mtime):
-		if dir_name not in os.listdir(path) or mtime > lu_time:
-			os.mkdir(path + dir_name)
-			#print('copied dir')
+	def backup_dir(dir_name, path, c_time, update_log):
+		if dir_name not in os.listdir(path):# or c_time > last_upt
+			os.mkdir(os.path.join(path, dir_name))
+			update_log.append(f'created dir - {os.path.join(path, dir_name)}')
+		return update_log
 
-	def backup_item(item, source, destination, mtime):
-		if item not in os.listdir(destination) or mtime > lu_time:
+	def backup_item(item, source, destination, m_time, update_log):
+		if item not in os.listdir(destination) or m_time > last_upt:
 			shutil.copy(source, destination)
-			#print('copied file')
+			update_log.append(f'copied file - {os.path.join(source)}')
+		return update_log
 
 	for item in items:
-		if os.path.isdir(path + item) and item.lower() not in ignore_dirs:
-			backup_dir(item, toplevel, os.path.getmtime(path + item))
-			new_path = path + item + "/"
-			check_items(new_path, toplevel + item + "/", lu_time, ignore_files, ignore_dirs)
-		elif os.path.splitext(item)[1] != '.ini' and item.lower() not in ignore_files and not os.path.isdir(path + item):
-			backup_item(item, path + item, toplevel, os.path.getmtime(path + item))
+		item_path = os.path.join(path, item)
+		if os.path.isdir(item_path) and item.lower() not in ignore_dirs:
+			update_log = backup_dir(item, top_level, os.path.getmtime(item_path), update_log)
+			update_log += check_items(item_path, os.path.join(top_level, item))
+		elif not os.path.isdir(item_path) and os.path.splitext(item)[1] != '.ini' and item.lower() not in ignore_files:
+			update_log = backup_item(item, item_path, top_level, os.path.getmtime(item_path), update_log)
+	
+	return update_log
 
-def remove_items(path, backup_path, ignore_files, ignore_dirs):
+def remove_items(path, backup_path):
 	items = os.listdir(backup_path)
+	remove_log = []
 	for item in items:
 		local_items = os.listdir(path)
-		if os.path.isdir(backup_path + item):
-			if item not in local_items and item.lower() not in ignore_dirs:
-				print(backup_path + item)
-				shutil.rmtree(backup_path + item)
-				#print('removed dir')
+		backup_item_path = os.path.join(backup_path, item)
+		if os.path.isdir(backup_item_path):
+			if item not in local_items or item.lower() in ignore_dirs:
+				try:
+					shutil.rmtree(backup_item_path)
+					remove_log.append(f'removed dir - {backup_item_path}')
+				except:
+					pass
 			else:
-				remove_items(path + item + '/', backup_path + item + '/', ignore_files, ignore_dirs)
-		elif item not in local_items and item.lower() not in ignore_files:
-			os.remove(backup_path + item)
-			#print('removed file')
+				remove_items(os.path.join(path, item), backup_item_path)
+		elif item not in local_items or item.lower() in ignore_files:
+			try:
+				os.remove(backup_item_path)
+				remove_log.append(f'removed file - {backup_item_path}')
+			except:
+				pass
+	return remove_log
 
+def write_to_log(backup_drive, update_log, remove_log):
+	with open(os.path.join(backup_drive, "log.txt"), "w") as f:
+		update_time = time.time()
+		f.write(f"Last update time: {update_time}\n")
+		timestamp = datetime.fromtimestamp(update_time)
+		f.write(f"{timestamp.strftime('%Y-%m-%d %H:%M:%S')}\n")
+		if len(update_log) == 0 and len(remove_log) == 0:
+			f.write("\nAll files up to date.\n")
+		else:
+			if len(update_log) != 0:
+				f.write("\nUpdates:\n")
+				for update in update_log:
+					f.write(f"{update}\n")
+
+			if len(remove_log) != 0:
+				f.write("\nRemoved:\n")
+				for remove in remove_log:
+					f.write(f"{remove}\n")
 
 if __name__ == '__main__':
-	run = 1
-	ignore_files = []
-	ignore_dirs = ['github']
-	backup_dirs = ['Desktop/', 'Documents/']
-	path = 'C:/Users/hitts/'
-	backup_path = 'D:/'
-	while run:
-		time.sleep(20)
-		lu_time = time.time()
-		available_drives = ['%s:/' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
-		if backup_path in available_drives:
-			for backup_dir in backup_dirs:
-				if not os.path.isdir(backup_path + backup_dir):
-					os.mkdir(backup_path + backup_dir)
-				check_items(path + backup_dir, backup_path + backup_dir, lu_time, ignore_files, ignore_dirs)
-				remove_items(path + backup_dir, backup_path + backup_dir, ignore_files, ignore_dirs)
-		#time.sleep(3600)
-		run = 0
+	argparser = argparse.ArgumentParser()
+	argparser.add_argument(
+		"--backup-loc",
+		default="E",
+		type=str)
+	argparser.add_argument(
+		"-v", 
+		"--verbose",
+		action="store_true")
+	args = argparser.parse_args()
+
+	backup_drive = args.backup_loc + ":\\"
+
+	if args.verbose:
+		print(f"Backup drive: \"{backup_drive}\"")
+		if target_dirs:
+			print("Default target directories: ", "\\, ".join(target_dirs) + "\\")
+		if ignore_files:
+			print("Ignoring files: ", ", ".join(ignore_files))
+		if ignore_dirs:
+			print("Ignoring directories: ", "\\, ".join(ignore_dirs) + "\\")
+
+	last_upt = 0
+	log_file = os.path.join(backup_drive, "log.txt")
+	if os.path.exists(log_file):
+		f = open(log_file, 'r')
+		last_upt = float(re.search("[0-9]*\\.[0-9]*$", f.readline()).group(0))
+		f.close()
+		os.remove(log_file)
+
+	path = 'C:\\Users\\hitts\\'
+	update_log = []
+	remove_log = []
+	if True:
+		available_drives = ["%s:\\" % d for d in string.ascii_uppercase if os.path.exists("%s:" % d)]
+		if backup_drive in available_drives:
+			for target_dir in target_dirs:
+				backup_target_dir = os.path.join(backup_drive, target_dir)
+				local_target_dir = os.path.join(path, target_dir)
+				if not os.path.isdir(backup_target_dir):
+					os.makedirs(os.path.join(backup_drive, target_dir))
+				update_log += check_items(local_target_dir, backup_target_dir)
+				remove_log += remove_items(local_target_dir, backup_target_dir)
+	write_to_log(backup_drive, update_log, remove_log)
